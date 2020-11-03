cee66a3f1073dc5f8f509a7aa5192fe4152f8f53
timodokter <70761760+timodokter@users.noreply.github.com>
2020-11-03 10:09:09 +0100

    hst 3 is af maar geluid werkt niet

--- Garp/GarpsWorld.java
+++ Garp/GarpsWorld.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class GarpsWorld extends World
 {
-
+    private GreenfootSound sound;
     /**
      * Constructor for objects of class GarpsWorld.
      * 
@@ -17,5 +17,28 @@ public class GarpsWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(700, 500, 1); 
+        populateTheWorld();
+    }
+    
+    protected void populateTheWorld() {
+        addObject(new Garp(),350,250);
+        addObject(new Gnomus(),Greenfoot.getRandomNumber(670) + 15,Greenfoot.getRandomNumber(470) + 15);
+        sound = new GreenfootSound("Zelda.mp3");
+        for(int teller = 0; teller < 4; teller++) {
+            addObject(new Bomb(),Greenfoot.getRandomNumber(700),Greenfoot.getRandomNumber(500));
+        }
+        for(int teller = 0; teller < 6; teller++) {
+            addObject(new Rock(),Greenfoot.getRandomNumber(700),Greenfoot.getRandomNumber(500));
+        }
+        for(int teller = 0; teller < 10; teller++) {
+            addObject(new Diamant(),Greenfoot.getRandomNumber(700),Greenfoot.getRandomNumber(500));
+        }
+    }
+    
+    public void Started() {
+        sound.playLoop();
+    }
+    public void stopped() {
+        sound.stop();
     }
 }
--- Garp/Gnomus.java
+++ Garp/Gnomus.java
@@ -6,24 +6,20 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Gnomus extends Actor
-{
+public class Gnomus extends Actor {
+    public Gnomus() {
+        setRotation();
+    }
     /**
      * Act - do whatever the Gnomus wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
-    public void act() 
-    {   
+    public void act() {   
         int random;
-        
         move(5);
         if(atWorldEdge()) {
             move(-5);
-            if(Greenfoot.getRandomNumber(100) < 50) {
-                setRotation(getRotation() + Greenfoot.getRandomNumber(180));
-            } else {
-                setRotation(getRotation() - Greenfoot.getRandomNumber(180));
-            }
+            setRotation();
         } else {
             random = Greenfoot.getRandomNumber(100);
             if (random < 2){
@@ -35,17 +31,30 @@ public class Gnomus extends Actor
         }
     }
     private boolean atWorldEdge() {
-        int x, i, wx;
+        int x, i, wx, y, wy;
         
         x = getX();
+        y = getY();
         i = getImage().getWidth() / 2;
         wx = getWorld().getWidth() - i;
+        wy = getWorld().getHeight() - i;
         if(x <= i) {
             return true;
-        }
-        if(x >= wx) {
+        } else if(y <= i) {
             return true;
+        } else {
+            if (x >= wx) {
+                return true;
+            } else {
+                return y >= wy;
+            }
         }
-        return false;
+    }
+    protected void setRotation() {
+        if(Greenfoot.getRandomNumber(100) < 50) {
+            setRotation(getRotation() + Greenfoot.getRandomNumber(180));
+        } else {
+            setRotation(getRotation() - Greenfoot.getRandomNumber(180));
+        }
     }
 }
